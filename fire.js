5b0507e8aa2a2940b80014737386cbfe012d9032
maldinipunisher <maldinipunisher@gmail.com>
2022-08-07 15:42:33 +0700

    Update fire.js

--- fire.js
+++ fire.js
@@ -1,14 +1,15 @@
 var firebase = require('firebase')
-var config = {
-    apiKey: "AIzaSyB2JRWlQknMpl5Ptzi_q_fWJrZs8pT_S2c",
-    authDomain: "bots-fed6f.firebaseapp.com",
-    databaseURL: "https://bots-fed6f-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "bots-fed6f",
-    storageBucket: "bots-fed6f.appspot.com",
-    messagingSenderId: "444989246810",
-    appId: "1:444989246810:web:384ac3b15cc07faf317763",
-    measurementId: "G-KF5F80DXPF"
-};
+// var config = {
+//     apiKey: "Aizn21bjkabdwa",
+//     authDomain: "bots-fed6f.firebaseapp.com",
+//     databaseURL: "https://bots-fed6f-default-rtdb.asia-southeast1.firebasedatabase.app",
+//     projectId: "bots-fed6f",
+//     storageBucket: "bots-fed6f.appspot.com",
+//     messagingSenderId: "2114231411321",
+//     appId: "1:41231253221313",
+//     measurementId: "G-WDAJKDSAF"
+// };
+var config = YOURCONFIG
 
 var fire = firebase.initializeApp(config);
 module.exports = fire
